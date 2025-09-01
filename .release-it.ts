cd12c5d4507d15abfc5e3ce35c7599d603d62239
☁️淡然 <hi@ydr.me>
2025-09-02 05:26:18 +0800

    build(release): 更新 release-it 配置
    - 添加 tagMatch 正则，规范版本标签格式
    - 移除 infile 配置项，使用默认变更日志文件

--- .release-it.ts
+++ .release-it.ts
@@ -4,6 +4,7 @@ export default {
   git: {
     commit: true,
     tag: true,
+    tagMatch: 'v[0-9]*.[0-9]*.[0-9]*',
     push: true,
     requireCleanWorkingDir: false,
     commitMessage: 'chore(release): v5.x',
@@ -23,7 +24,6 @@ export default {
   plugins: {
     '@release-it/conventional-changelog': {
       preset: 'angular',
-      infile: 'CHANGELOG.md',
     },
   },
 } satisfies Config;
